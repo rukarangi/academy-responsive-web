445d675aec880bc80c0bf0a8bcd486ed4ce94fe2
Luke Thompson <luke@yorkbay.net.nz>
2021-01-18 16:13:20 +1300

    Adding Votes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ var app = new Vue({
     el: ".content",
 
     data: {
-        movies: []
+        movies: {},
+				people: [],
+				user: 1
     },
 
     created: function() {
@@ -13,12 +15,55 @@ var app = new Vue({
         fetchData: function() {
             var self = this;
             var list = "/api/";
+						var votes = "/api/votes";
+						var people = "/api/people";
 
             fetch(list).then(function(response) {
               	return response.json();
             }).then(function(json) {
-              	self.movies = json.movies;
-            });
-        }
+              	var movies = {};
+
+								json.movies.forEach(function(movie) {
+										movie.votes = 0;
+										movies[movie.id] = movie;
+								});
+
+								self.movies = movies;
+
+								fetch(votes).then(function(voteResponse) {
+										return voteResponse.json();
+								}).then(function(voteJson) {
+										voteJson.votes.forEach(function(vote) {
+												var id = vote.movie.id;
+												var movie = self.movies[id];
+
+												Vue.set(movie, "votes", movie.votes + 1);
+										});
+								});
+						});
+
+						fetch(people).then(function(peopleResponse) {
+								return peopleResponse.json();
+						}).then(function(peopleJson) {
+								self.people = peopleJson.people;
+						});
+        },
+
+				sendVote: function(id) {
+						var self = this;
+						var voteEndpoint = "/api/vote/";
+
+						fetch(
+								voteEndpoint + self.user + "/" + id,
+								{method: "POST"}
+						).then(function(response) {
+								if (response.ok) {
+										Vue.set(self.movies[id], 'votes', self.movies[id].votes + 1);
+								} else {
+										alert('You have already voted for this!');
+								}
+						})
+
+				}
     }
 })
